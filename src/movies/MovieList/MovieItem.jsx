18b8bde92466a88cfa64e974e098692b7c43c612
Simon Bächler <simon.baechler@ti8m.ch>
2022-09-15 14:07:06 +0200

    Add link

--- src/movies/MovieList/MovieItem.jsx
+++ src/movies/MovieList/MovieItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { IMAGE_BASE_URL } from "../../constants";
+import {Link} from 'react-router-dom';
 
 /**
  * A single item of the movie list.
@@ -9,9 +10,11 @@ export function MovieItem({movie}) {
   const posterPath = `${IMAGE_BASE_URL}/w342/${movie.poster_path}`;
 
   return (
-    <div className="relative">
-      <img src={posterPath} alt="" />
-      <p className="absolute inset-x-0 bottom-0 bg-white bg-opacity-95 px-1 py-0.5">{movie.title}</p>
-    </div>
+    <Link to={String(movie.id)}>
+      <div className="relative">
+        <img src={posterPath} alt="" />
+        <p className="absolute inset-x-0 bottom-0 bg-white bg-opacity-95 px-1 py-0.5">{movie.title}</p>
+      </div>
+    </Link>
   )
 }
