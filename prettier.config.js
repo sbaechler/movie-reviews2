cb35fa80fdc49e34b358c94f2ebba53258a37214
Simon Bächler <simon.baechler@ti8m.ch>
2024-04-05 10:20:11 +0200

    add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,28 @@
+export default {
+    printWidth: 100,
+    tabWidth: 2,
+    singleQuote: true,
+    trailingComma: 'all',
+    arrowParens: 'always',
+    overrides: [
+        {
+            files: '*.{json,css,scss,yml,yaml,html}',
+            options: {
+                tabWidth: 2,
+                singleQuote: false,
+            },
+        },
+        {
+            files: '*.json',
+            options: {
+                printWidth: 999999,
+            },
+        },
+        {
+          files: '*.md',
+          options: {
+            tabWidth: 4,
+          },
+        },
+    ],
+};
